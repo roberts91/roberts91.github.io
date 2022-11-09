106b146b4067d26eda6431bfe76f298d4cb34218
Robert Sæther <robsat91@gmail.com>
2022-11-09 02:27:24 +0100

    Minor fix

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: 'src/index.html'
+        template: 'src/index.html',
+        scriptLoading: 'blocking'
     })
   ],
   module: {
